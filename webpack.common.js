f1226186cd583c1bf8bb3f28b6e07dfc7944b542
wenzheng1 <wenzhenghaha@163.com>
2019-03-21 17:51:59 +0800

    change code style

--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,34 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: {
-    app: './src/app.js',
-    index : './src/index.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename : 'index.htm',
-      template: './src/index.htm',
-      inject : false,
-      hash : true,
-
-    }),
-    new HtmlWebpackPlugin({
-      filename : 'app.htm',
-      template: './src/app.htm',
-      inject : false
-    })
-  ],
-  output: {
-    filename: '[name]-[hash].js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
+    entry: {
+        app: './src/app.js',
+        index : './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename : 'index.htm',
+            template: './src/index.htm',
+            inject : false,
+            hash : true,
+        }),
+        new HtmlWebpackPlugin({
+            filename : 'app.htm',
+            template: './src/index.htm',
+            inject : false
+        })
+    ],
+    output: {
+        filename: '[name]-[hash].js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
         rules : [{
             test: /\.less$/,
             use : ['style-loader' , 'css-loader' , 'less-loader']
-        },{
+        },
+        {
             test: /\.(js|jsx)$/,
             use: {
                 loader: 'babel-loader',
--- webpack.product.js
+++ webpack.product.js
@@ -3,9 +3,9 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 module.exports = merge(common, {
     devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
+    plugins: [
+        new UglifyJSPlugin({
         sourceMap: true
     })
-  ]
+    ]
 });
